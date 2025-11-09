c4225c6c6aa0c457dd859fc5dcce59303df6aecd
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-11-09 14:14:17 +0200

    Create necessary services and interfaces needed for auditing

---
+++ src/MFO.PartnerManagementService.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using MFO.PartnerManagementService.Application.Interfaces;
+
+namespace MFO.PartnerManagementService.Infrastructure.Services;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
