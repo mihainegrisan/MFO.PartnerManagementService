ba1b1b8c3aa9068b4ca264fe06672064dd0fb9b2
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-11-09 14:20:29 +0200

    Register services

--- src/MFO.PartnerManagementService.API/Program.cs
+++ src/MFO.PartnerManagementService.API/Program.cs
@@ -1,9 +1,16 @@
+using MFO.PartnerManagementService.Application.Interfaces;
+using MFO.PartnerManagementService.Application.Interfaces.Repositories;
+using MFO.PartnerManagementService.Infrastructure.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddControllers();
+
+builder.Services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+builder.Services.AddScoped<IUserContextProvider, UserContextProvider>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,28 +21,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+await app.RunAsync();
