713029715492774c639e2956f8c23f3c66d82a23
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-11-25 07:40:28 +0200

    Fix #4 Add GetPartnerDetailsForValidationQuery, Handler and Validator

---
+++ src/MFO.PartnerManagementService.Application/Features/Queries/GetPartnerDetailsForValidationQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace MFO.PartnerManagementService.Application.Features.Queries;
+
+public class GetPartnerDetailsForValidationQueryValidator : AbstractValidator<GetPartnerDetailsForValidationQuery>
+{
+    public GetPartnerDetailsForValidationQueryValidator()
+    {
+        RuleFor(q => q.PartnerId)
+            .NotNull()
+            .NotEmpty();
+    }
+}
