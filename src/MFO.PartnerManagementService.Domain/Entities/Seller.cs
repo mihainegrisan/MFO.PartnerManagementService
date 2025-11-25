61342bed1aa910f8ec83eb62aa2f343ead3aa0da
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-11-25 07:43:10 +0200

    Update Entities and enums

--- src/MFO.PartnerManagementService.Domain/Entities/Seller.cs
+++ src/MFO.PartnerManagementService.Domain/Entities/Seller.cs
@@ -12,7 +12,7 @@ public class Seller : AuditableEntity
     public required Guid PartnerId { get; set; }
 
     // Current operational status as a marketplace seller
-    public required SellerStatus SellerStatus { get; set; } = SellerStatus.InReview;
+    public required SellerStatus SellerStatus { get; set; }
 
     // Links to the Logistics Hub service (e.g., a reference ID for their primary fulfillment location)
     public required Guid DefaultWarehouseId { get; set; }
