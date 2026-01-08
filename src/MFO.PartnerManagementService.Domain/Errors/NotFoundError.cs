f3802112272a359aa3cf5eb74ec5cb7fd28fffb9
Mihai Negrisan <mihainegrisan1@gmail.com>
2026-01-08 18:16:04 +0200

    Add NotFoundError

---
+++ src/MFO.PartnerManagementService.Domain/Errors/NotFoundError.cs
@@ -0,0 +1,13 @@
+﻿
+using FluentResults;
+
+namespace MFO.PartnerManagementService.Domain.Errors;
+
+public class NotFoundError : Error
+{
+    public NotFoundError(string message) : base(message)
+    {
+        Metadata.Add("ErrorType", "NotFound");
+        Metadata.Add("StatusCode", 404);
+    }
+}
