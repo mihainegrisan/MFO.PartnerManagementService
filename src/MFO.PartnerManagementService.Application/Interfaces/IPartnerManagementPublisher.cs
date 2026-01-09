68ea8ad2ba0273e7e241fbafc52e6b84d4cdbacc
Mihai Negrisan <mihainegrisan1@gmail.com>
2026-01-09 10:38:01 +0200

    Add PartnerManagementPublisher

---
+++ src/MFO.PartnerManagementService.Application/Interfaces/IPartnerManagementPublisher.cs
@@ -0,0 +1,9 @@
+﻿using MFO.PartnerManagementService.Application.Events;
+
+namespace MFO.PartnerManagementService.Application.Interfaces;
+
+public interface IPartnerManagementPublisher
+{
+    Task PublishSellerRegisteredEvent(SellerRegisteredEvent sellerRegisteredEvent);
+    Task PublishSellerStatusUpdatedEvent(SellerStatusUpdatedEvent sellerStatusUpdatedEvent);
+}
---
+++ src/MFO.PartnerManagementService.Infrastructure/Kafka/PartnerManagementPublisher.cs
@@ -0,0 +1,42 @@
+﻿using MFO.PartnerManagementService.Application.Events;
+using MFO.PartnerManagementService.Application.Interfaces;
+
+namespace MFO.PartnerManagementService.Infrastructure.Kafka;
+
+public class PartnerManagementPublisher : IPartnerManagementPublisher
+{
+    private const string SellerRegisteredTopic = "partner-management-seller-registered";
+    private const string SellerStatusUpdatedTopic = "partner-management-seller-status-updated";
+    private readonly IKafkaProducer _producer;
+
+    public PartnerManagementPublisher(IKafkaProducer producer)
+    {
+        _producer = producer;
+    }
+
+    public async Task PublishSellerRegisteredEvent(SellerRegisteredEvent sellerRegisteredEvent)
+    {
+        var key = sellerRegisteredEvent.PartnerId;
+
+        await _producer.ProduceAsync(
+            SellerRegisteredTopic,
+            key,
+            sellerRegisteredEvent
+        );
+
+        Console.WriteLine($"Published Seller Registered Event for Company: '{sellerRegisteredEvent.CompanyName}'.");
+    }
+
+    public async Task PublishSellerStatusUpdatedEvent(SellerStatusUpdatedEvent sellerStatusUpdatedEvent)
+    {
+        var key = sellerStatusUpdatedEvent.PartnerId;
+
+        await _producer.ProduceAsync(
+            SellerStatusUpdatedTopic,
+            key,
+            sellerStatusUpdatedEvent
+        );
+
+        Console.WriteLine($"Published Seller Status Updated Event for PartnerId: '{sellerStatusUpdatedEvent.PartnerId}' with Status: '{sellerStatusUpdatedEvent.SellerStatus}'.");
+    }
+}
