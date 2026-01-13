4e1c546336528f0e1f42d80d80aed10c3a5d47d4
Mihai Negrisan <mihainegrisan1@gmail.com>
2026-01-13 10:43:22 +0200

    Add missing DTOs

---
+++ src/MFO.PartnerManagementService.Application/DTOs/PartnerValidationDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using MFO.PartnerManagementService.Domain.Enums;
+
+namespace MFO.PartnerManagementService.Application.DTOs;
+
+public sealed record PartnerValidationDetailsDto
+{
+    public required Guid PartnerId { get; init; }
+    public required string DisplayName { get; init; }
+    public required OnboardingStatus OnboardingStatus { get; init; }
+    public required SellerStatus SellerStatus { get; init; }
+    public required Guid DefaultWarehouseId { get; init; }
+}
